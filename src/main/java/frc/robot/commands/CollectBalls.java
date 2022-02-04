e9ccd8625b1e1cafa087fea9866e70569bcf3c50
lhs-robotics <lindberghrobotics@gmail.com>
2022-02-04 15:17:01 -0800

    changed joystick reading-jo

--- src/main/java/frc/robot/commands/CollectBalls.java
+++ src/main/java/frc/robot/commands/CollectBalls.java
@@ -54,7 +54,7 @@ public class CollectBalls extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() { 
-        m_acquisition.setAcquisitionMotor(RobotContainer.getInstance().getoperatorController().getLeftY());
+        m_acquisition.setAcquisitionMotor(RobotContainer.getInstance().getoperatorController().getRawAxis(1));
 
     }
 
